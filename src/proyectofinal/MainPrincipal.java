07a08f68ee3ec1d54f9ec8cf280de09fbc2c3c29
Charnel Morales <ftumm1@miumg.edu.gt>
2025-09-13 12:04:27 -0600

    Initial commit of Netbeas Project

---
+++ src/proyectofinal/MainPrincipal.java
@@ -0,0 +1,25 @@
+package proyectofinal;
+import Conexion.CreateConection;
+import java.sql.SQLException;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+
+public class MainPrincipal {
+
+    public static void main(String[] args) throws SQLException {
+        CreateConection conexionPostgres = new CreateConection();
+        Connection con= conexionPostgres.getConection();
+        try {
+            Statement smt = con.createStatement();
+            String qry = "insert into public.empleado(nombre,apellido,salario) " + " values ('Marina', 'Catalan', 7550.25)";
+            int filasInsertadas = smt.executeUpdate(qry);
+            System.out.println("Total de Registros Insertados " + filasInsertadas);
+            smt.close();
+        } catch (SQLException e){
+            e.getMessage();
+        }
+    }
+    
+}
